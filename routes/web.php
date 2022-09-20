639cd7820fae6e016bb7f11dc9148537dbe7c219
wafiputra <wafi.putra.arya@gmail.com>
2022-09-20 15:01:50 +0700

    Pusher Done v1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    event(new App\Events\MyEvent('Someone'));
+    return "Event has been sent!";
+});
